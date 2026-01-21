b1edca4df251b1975dd2f4659d0ee2be7f8dda65
kaihira1 <yunusemretuncer111@gmail.com>
2026-01-21 23:52:46 +0300

    should fail now

--- AspWebProject.Tests/UnitTest1.cs
+++ AspWebProject.Tests/UnitTest1.cs
@@ -5,7 +5,7 @@ public class UnitTest1
     [Fact]
     public void CI_should_fail_here()
     {
-        Assert.True(true); // ✅ intentional failure
+        Assert.True(false); // ✅ intentional failure
     }
 }
 
