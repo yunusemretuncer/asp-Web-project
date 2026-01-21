a1adb9a0cb0182e155b5f6a03835b1cce8f28b0f
kaihira1 <yunusemretuncer111@gmail.com>
2026-01-21 23:43:46 +0300

    fix cı fail to showcase cı gate works

--- AspWebProject.Tests/UnitTest1.cs
+++ AspWebProject.Tests/UnitTest1.cs
@@ -5,7 +5,7 @@ public class UnitTest1
     [Fact]
     public void CI_should_fail_here()
     {
-        Assert.True(false); // ✅ intentional failure
+        Assert.True(true); // ✅ intentional failure
     }
 }
 
